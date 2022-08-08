f69e4176e1f83ae25d85c18c6d5dc9c4477ec0af
Shawn0Banner <sayantan2295@gmail.com>
2022-08-08 09:54:45 +0530

    WIP

---
+++ RMSInternalPHP.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+    <body>
+<!-- ********************** Begin Pega content ********************** -->
+<script src ='https://exavalu01.pegalabs.io/prweb/PRServlet/app/exarms_0990/?pzuiactionzzz=CXtpbn02b2MzZWZydWlsd2V6ODZ3VnhiZE4yZk5Lb3N2NUhWNnhQRUJYZWxFem5jZ1hRSGE5RmNuV0RuOHRKN1pWTUpD*'></script>
+
+<div data-pega-encrypted ='true' 
+data-pega-encrypted-hash = 'pzuiactionzzz=CXtpbn1HRUNzUWJCTWVMVXFwU2VhVkhUd1Bkbi9UMmhwYkJDRHhXcDlNbURZNGlwU1EyYzd2UmJuM21McURyMWh0ZnF2amVxZStheXFyK2Q1a3U3TVNRak50TXVIN3VpdDYrWkY4T2lkbXZiYnJmMkJrUUhkWEFDbWJvZFJOUks3aW5yQ2MzdTZmaUw4d1E0L3lXVGZnNUc1VkxTNWFSM0prZXlxL1ZYdktCNDlTVXl3MHFEaFN4UXNQUnQwM2Jac2dpUkNYOWZ2TVI0QkkzeEhKZzJFVktRdzdqWXQ5VDhVTytQWjFiNlRQUStiVUh6bmRBSGY2dHkrMS9JOVAwcUZOaWhFczdtdWFCelBTOEJPUkYzckdPZFNCM3B6QnI5WDhrSEN0Wk1tLzZmam1kbWZzU2JseUR4S3E1UVJ3cmtLdDE3TkZOWkp4ZVpkQ3ZNZ05pRU9CTEtCd2c9PQ%3D%3D*' 
+data-pega-gadgetname ='PegaGadget' 
+data-pega-action ='display' 
+data-pega-action-param-harnessname ='RMS' 
+data-pega-action-param-classname ='Data-Portal' 
+data-pega-action-param-model ='' 
+data-pega-action-param-readonly ='false' 
+data-pega-isdeferloaded ='false' 
+data-pega-applicationname ='RMS' 
+data-pega-threadname ='RMSMashup' 
+data-pega-channelID ='MASHUPd00b7dd2fac842e68a0a0d1911c7ef4b' 
+data-pega-resizetype ='stretch' 
+data-pega-url ='https://exavalu01.pegalabs.io/prweb/PRServlet/app/exarms_0990/' 
+data-pega-action-param-parameters ='{"pzSkinName":"RMS","pyMashupSkeletonName":"pyDefaultMashupSkeleton"}' ></div>
+
+<!-- ********************** End Pega content ********************** -->
+</body>
+</html>
